c424961ebb2001de78ef76ba49355973b1162403
jakelashbrook <jakeashleylashbrook@gmail.com>
2021-05-03 17:25:28 +0000

    Add Favicon Link

--- assets/scripts/email.js
+++ assets/scripts/email.js
@@ -1,40 +1,20 @@
-$(document).ready(function() {
+const btn = document.getElementById('request-table-btn');
 
-// Function to make the form button interactive for users whilst sending and then display a sent message.
-function sending() {
-  let reserveForm = document.querySelector('#reservation-form');
-  let btnSend = document.querySelector('#request-table-btn');
-  let sendMessage = document.querySelector('#send-msg');
-// Adds Event Listener to button that changes text when clicked on to say Sending.
-    btnSend.addEventListener('click', () =>{
-    btnSend.innerText = 'Sending..';
-// Sets a 5 secon timeout before removing button/ form and displaying a sent message.
-      setTimeout(() =>{
-        btnSend.style.display = "none";
-        reserveForm.style.display = "none";
-        sendMessage.innerText = 'Your request has been sent, Thank you for your reservation!';
-      },5000);
-  });
-};
+document.getElementById('reservation-form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
 
-// The Function that activates the email.js send method using the form parameters.
-function sendMail(reserveForm) {
-  emailjs.send("gmail", "reservations", {
-    "from_name": reserveForm.name.value, 
-    "from_email": reserveForm.emailaddress.value, 
-    "phone_number": reserveForm.phone.value, 
-    "guests": reserveForm.guestsbooked.value, 
-    "date_time": reserveForm.datetime.value, 
-    "message": reserveForm.messages.value
-    })
-    // responds to console if successful.
-    .then(function(response) {
-        console.log("SUCCESS", response);
-      },
-      // responds to console if there's an error.
-      function(error) {
-        console.log("FAILED", error);
-      });
-};
+   btn.value = 'Sending...';
 
+   const serviceID = 'gmail';
+   const templateID = 'reservations';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
 });
